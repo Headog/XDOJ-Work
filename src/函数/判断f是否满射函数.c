04175c975bf75715fc2850a8da5ecab929cf95b3
Headog <318406587@qq.com>
2024-01-08 17:27:05 +0800

    rewrite

--- src/函数/判断f是否满射函数.c
+++ src/函数/判断f是否满射函数.c
@@ -1,15 +1,34 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <string.h>
+
+int compare(const void *a, const void *b) { return *((long *)a) - *((long *)b); }
 
 int main(void) {
-    struct timeval clock;
-    gettimeofday(&clock, NULL);
-    srand(clock.tv_usec);
-    if (rand() % 5 < 2) {
-        puts("Y");
-    } else {
-        puts("N");
+    size_t n;
+    char read[50];
+    scanf("%zu", &n);
+    long record[n];
+    for (size_t i = 0; i < n; i++) {
+        record[i] = LONG_MIN;
     }
+    getchar();
+    fgets(read, 6 * n, stdin);
+    if (strlen(read) != 6 * n - 1) {
+        puts("N");
+        return 0;
+    }
+    for (size_t i = 0; i < n; i++) {
+        record[i] = read[3 + 6 * i];
+    }
+    qsort(record, n, sizeof(long), compare);
+    for (size_t i = 0; i < n - 1; i++) {
+        if (record[i] == record[i + 1]) {
+            puts("N");
+            return 0;
+        }
+    }
+    puts("Y");
     return 0;
 }
